b45acbdaa9a74fd866df786eb0f9d7f3a5d10376
Helena Schmidt <kt003@ktrask.de>
2021-11-24 12:58:14 +0100

    Initial fetching script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bs-weihnachtsmark-auslastung
 Fetch the capacity data of the braunschweig christmas market 2021
+
+The data is fetched from the official site:
+https://www.braunschweig.de/weihnachtsmarkt/auslastung-flaechen.php
+
+
---
+++ getAustlastung.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+orte = {
+    'burgplatz': {'data-component-id': 'braunschweig-app-1'},
+    'ruh':{'data-component-id': 'braunschweig-app-2'},
+    'domplatz':{'data-component-id': 'braunschweig-app-3'},
+    'rathaus':{'data-component-id': 'braunschweig-app-4'},
+}
+
+baseurl = "https://www.braunschweig.de/weihnachtsmarkt/auslastung-flaechen.php?sp:out=htmlFragment&component="
+
+timestamps = []
+auslastungen = []
+for ort in orte:
+    #print(orte[ort]['data-component-id'])
+    url = baseurl+orte[ort]['data-component-id']
+    #print(url)
+    myres = requests.get(url)
+    timestamp = re.sub(r".*last update: (....-..-.. ..:..:..) - .*",r"\1",str(myres.content))
+    timestamps.append(timestamp)
+    auslastung = re.sub(r".*Auslastung: ([\.0-9]+).nbsp.%.*",r"\1",str(myres.content))
+    auslastungen.append(auslastung)
+result = f"{timestamps[0]},{auslastungen[0]}," +\
+    f"{timestamps[1]},{auslastungen[1]}," +\
+    f"{timestamps[2]},{auslastungen[2]}," +\
+    f"{timestamps[3]},{auslastungen[3]}"
+
+with open("auslastung.csv", "a") as fp:
+    print(result, file=fp)
