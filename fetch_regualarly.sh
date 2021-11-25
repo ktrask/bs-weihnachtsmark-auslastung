00473ddf0373b075997d64472ad6507cd568d55c
Helena Schmidt <kt003@ktrask.de>
2021-11-25 19:28:45 +0100

    removed auslastung.csv from git

--- fetch_regualarly.sh
+++ fetch_regualarly.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [[ ! -f auslastung.csv ]]
+then
+    cp auslastung_dummy.csv auslastung.csv
+fi
+
 while :
 do
   date
   python3 getAuslastung.py
   sleep 60
 done
-getAustlastung.py
